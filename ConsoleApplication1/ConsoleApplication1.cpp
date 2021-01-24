987133b9676e8586233f221a25f88abc28640b69
Darinochka <darisha6602@gmail.com>
2021-01-24 17:02:53 +0500

    map1

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,103 +1,27 @@
 ﻿#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-
+#include <map>
 using namespace std;
 
-void add(vector <vector <string>>& work, int& q, string plan, string date) {
-	int i;
-	for (i = 0; i < work.size() && work[i][0] != date; i++);
-	if (i < work.size()) {
-		work[i].push_back(plan);
+map <char, int> BuildCharCounters (string word) {
+	map <char, int> result;
+	for (auto i: word) {
+		++result[i];
 	}
-	else {
-		work.resize(q + 1);
-		work[q].push_back(date);
-		work[q].push_back(plan);
-		q++;
-	}
+	return result;
 }
-
-void sort(vector<vector<string>>& work) {
-	bool key = true;
-	while (key) {
-		key = false;
-		for (int i = 0; i < work.size()-1; i++) {
-			if (stoi(work[i][0]) > stoi(work[i + 1][0])) {
-				vector <string> temp = work[i];
-				work[i] = work[i + 1];
-				work[i + 1] = temp;
-				key = true;
-			}
-		}
-	}
-	
-}
-
 int main() {
-	int n, i, j, q = 0, month = 1;
-	string command, plan, date;
-	bool key;
-	vector <vector <string>> work;
-	vector <int> next_30 = { 3, 5, 8, 10 };
-
+	int n;
+	string word1, word2;
 	cin >> n;
-
-	for (int iter = 0; iter < n; iter++) {
-		cin >> command;
-
-		if (command == "ADD") {
-			cin >> date >> plan;
-			add(work, q, plan, date);
+	for (int i = 0; i < n; i++) {
+		cin >> word1 >> word2;
+		if (BuildCharCounters(word1) == BuildCharCounters(word2)) {
+			cout << "YES";
 		}
-
-		if (command == "DUMP") {
-			cin >> date;
-			key = false;
-			for (auto plans : work) {
-				if (plans[0] == date) {
-					cout << plans.size() - 1 << " ";
-					for (i = 1; i < plans.size(); i++)
-						cout << plans[i] << " ";
-					key = true;
-					cout << endl;
-				}
-			}
-			if (!key)
-				cout << "0" << endl;
-		}
-
-		if (command == "NEXT") {
-			string last_day = "31";
-
-			auto found = find(next_30.begin(), next_30.end(), month);
-			if (month == 1)
-				last_day = "28";
-			else if (found != next_30.end())
-				last_day = "30";
-
-			if (work.size() != 0) {
-				i = 0;
-				sort(work);
-				while (i < work.size()) {
-					if (stoi(work[i][0]) > stoi(last_day)) {
-						for (j = 1; j < work[i].size(); j++) {
-							add(work, q, work[i][j], last_day);
-						}
-						work.erase(work.begin() + i);
-						q--;
-						i--;
-					}
-					i++;
-				}
-			}
-			if (month == 12) {
-				month = 1;
-			}
-			else {
-				month++;
-			}
+		else {
+			cout << "NO";
 		}
 	}
+	return 0;
+
 }
