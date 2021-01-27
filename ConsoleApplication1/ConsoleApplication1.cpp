f8b016f4ecb2200bafa9cc0ee94872773a0cc6f0
Darinochka <darisha6602@gmail.com>
2021-01-28 01:18:38 +0500

    buses3

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,38 +3,36 @@
 #include <map>
 
 using namespace std;
-  
+
+int CheckStops(const map <int, set<string>>& stops, const set <string>& temp) {
+    for (const auto& [key, value] : stops) {
+            if (value == temp) {
+                return key;
+            }
+    }
+    return 0;
+}
 
 int main() {
-    int n;
-    string command, word1, word2;
-    set <set<string>> words;
-    map <string, int> count_syn;
+    int n, q = 0;
+    string stop;
     cin >> n;
+    map <int, set<string>> stops;
+    set <string> temp;
     for (int i = 0; i < n; i++) {
-        cin >> command;
-
-        if (command == "ADD") {
-            cin >> word1 >> word2;
-            if (words.find({word1, word2}) == words.end()) {
-                words.insert({word1, word2});
-                ++count_syn[word1];
-                ++count_syn[word2];
-            }
+        int count;
+        cin >> count;
+        temp.clear();
+        for (int j = 0; j < count; j++) {
+            cin >> stop;
+            temp.insert(stop);
         }
-        if (command == "COUNT") {
-            cin >> word1;
-            cout << count_syn[word1] << endl;
+        if (CheckStops(stops, temp) == 0) {
+            stops[++q] = temp;
+            cout << "New bus " << q << endl; 
+        } else {
+            cout << "Already exists for " << CheckStops(stops, temp) << endl;
         }
-        if (command == "CHECK") {
-            cin >> word1 >> word2;
-            if (words.find({word1, word2}) == words.end()) {
-                cout << "NO" << endl;
-            } else {
-                cout << "YES" << endl;
-            }
-        }
-
     }
     return 0;
 }
