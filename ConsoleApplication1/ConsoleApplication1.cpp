a61291f4999e43c462b0c94339b29efb229498a0
Darinochka <darisha6602@gmail.com>
2021-01-27 22:05:56 +0500

    synon

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,50 +1,40 @@
 ﻿#include <iostream>
-#include <vector>
+#include <set>
 #include <map>
 
 using namespace std;
-
-int CheckStop(const map <int, vector<string>>& stops, const vector <string>& c) {
-    bool flag = false;
-    for (const auto& [key, value] : stops) {
-        if (value.size() == c.size()) {
-            flag = true;
-            for (int i = 0; i < value.size(); i++) {
-                if (value[i] != c[i]) {
-                    flag = false;
-                    break;
-                }
-            }
-        }
-        if (flag) {
-            return key;
-        }
-    }
-    return -1;
-}
+  
 
 int main() {
-    int n, coun;
-    string temp;
-    map <int, vector<string>> stops;
-    vector <string> c;
+    int n;
+    string command, word1, word2;
+    set <set<string>> words;
+    map <string, int> count_syn;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> coun;
-        c.clear();
-        for (int j = 0; j < coun; j++) {
-            cin >> temp;
-            c.push_back(temp); 
-        }
-        if (CheckStop(stops, c) == -1) {
-            for (auto item : c) {
-                stops[i+1].push_back(item);
+        cin >> command;
+
+        if (command == "ADD") {
+            cin >> word1 >> word2;
+            if (words.find({word1, word2}) == words.end()) {
+                words.insert({word1, word2});
+                ++count_syn[word1];
+                ++count_syn[word2];
             }
-            cout << "New bus " << i + 1 << endl;;
         }
-        else {
-            cout << "Already exists for " << CheckStop(stops, c) << endl;
+        if (command == "COUNT") {
+            cin >> word1;
+            cout << count_syn[word1] << endl;
         }
+        if (command == "CHECK") {
+            cin >> word1 >> word2;
+            if (words.find({word1, word2}) == words.end()) {
+                cout << "NO" << endl;
+            } else {
+                cout << "YES" << endl;
+            }
+        }
+
     }
     return 0;
 }
