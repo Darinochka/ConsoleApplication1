024e9634ae7893ba5af5e204e760d0939aeaad93
Darinochka <darisha6602@gmail.com>
2021-02-25 21:57:31 +0500

    readymass

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -4,16 +4,15 @@
 using namespace std;
 
 int main() {
-  uint32_t n;
-  uint16_t w, d, h, r;
+  int n, w, d, h, r;
   uint64_t mass = 0;
   
   cin >> n >> r;
-  for (auto i = 0; i < n; i++) {
+  for (uint32_t i = 0; i < n; i++) {
     cin >> w >> d >> h;
-    mass += static_cast<uint64_t>(w * d * h * r);  
+    mass += static_cast<uint64_t>(w)* d * h;  
   }
 
-  cout << mass;
+  cout << mass * r;
   return 0;
 }
--- ConsoleApplication1/class.cpp
+++ ConsoleApplication1/class.cpp
@@ -9,9 +9,9 @@ int main() {
   uint64_t mass = 0;
   
   cin >> n >> r;
-  for (auto i = 0; i < n; i++) {
+  for (uint32_t i = 0; i < n; i++) {
     cin >> w >> d >> h;
-    mass += static_cast<uint64_t>(w * d * h) * r;  
+    mass += static_cast<uint64_t>(w)* static_cast<uint64_t>(d) * static_cast<uint64_t>(h) * static_cast<uint64_t>(r);  
   }
 
   cout << mass;
